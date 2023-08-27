5e05512f25e81322516f6b0d6985374285351d29
ApaBird <apa.birds@mail.ru>
2023-08-27 22:28:59 +0800

    0.2

--- Project/main.py
+++ Project/main.py
@@ -2,5 +2,10 @@ from ClearMessage import isBanWord, clearBanWord
 
 message = input("Введите текст: ")
 
+if not isBanWord(message):
+    print("Очистка текста.....")
+    message = clearBanWord(message)
 
+with open("message.txt", 'a') as file:
+    file.write(message)
 
